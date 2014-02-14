ada74cf6e07c9bc84fa218fa0502f97b2e23a176
HKMR <hirojik@yahoo.co.jp>
2014-02-14 18:38:26 +0900

    nodejs htmlファイルを読み込み

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,28 +1,24 @@
 
 var http = require('http');
+var fs   = require('fs');
 var settings = require('./settings');
 console.log(settings);
 var server = http.createServer();
 
 server.on('request', function(req, res) {
+	fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data){
+		if(err){
+			res.writeHead(404, {'Content-Type:':'text/plain'});
+			res.write('not found');
+			return res.end();
 
-	switch(req.url){
-		case '/about':
-			msg = "about this page";
-			break;	
-		case '/profile':
-			msg = 'about me';
-			break;
-		default:
-			msg = 'wrong page';
-			break;
-	}
-
-	res.writeHead(200, {'Content-Type:':'text/plain'});
-	res.write(msg);
-
-	res.end();
+		}
+		res.writeHead(200, {'Content-Type:':'text/html'});
+		res.write(data);
+		res.end();
+	});
 });
+
 
 server.listen(settings.port, settings.host);
 console.log("server listening...");
