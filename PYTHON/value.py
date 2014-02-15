1675c69e944717ba43c5b8ca93e06b607771176a
HKMR <hirojik@yahoo.co.jp>
2014-02-15 19:19:14 +0900

    数値、文字列

---
+++ PYTHON/value.py
@@ -0,0 +1,26 @@
+# coding: UTF-8
+
+#演算子
+#	//	商
+#	**	べき乗
+
+print 10**2				# 100
+print 10//3				# 3
+
+print 5 + 2.0 			# 7.0
+
+print 5/3				# 1
+print 5/3.0				# 1.666..
+
+#文字列 
+
+msg = u"こんにちは\t"   # 文字列の長さの時などに使える
+
+print msg * 5
+
+print """ <html lang="ja">
+<body>
+</body>
+</html>"""
+
+
