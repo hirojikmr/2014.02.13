e25003514b5fc65508230e0d8cd1070e0792850f
HKMR <hirojik@yahoo.co.jp>
2014-02-18 01:28:40 +0900

    GAE Python 時間を表示する

--- PYTHON/helloworld/helloworld.py
+++ PYTHON/helloworld/helloworld.py
@@ -1,11 +1,34 @@
+#coding:UTF-8
 import webapp2
+import datetime
+import locale
 
+locale.setlocale(locale.LC_ALL, 'da_DK.UTF-8')
+
+HTML = """\
+	<!DOCTYPE html>
+	<html lang="ja">
+	<head>
+	<meta charset="UTF-8">
+	</head>
+	<body>
+	%s
+	%s
+	</body>
+	</html>
+"""
 
 class MainPage(webapp2.RequestHandler):
 
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+
+	def get(self):
+		#self.response.headers['Content-Type'] = 'text/html'
+		greet_str = '<h1>Hello, World!</h1>'
+
+		d = datetime.datetime.today()
+		time_str = '<h2>%s時%s分%s.%s秒<h2>' % (d.hour, d.minute, d.second, d.microsecond)
+
+		self.response.write(HTML % ( greet_str, time_str))
 
 
 application = webapp2.WSGIApplication([
--- RPI/read.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-cnt = 0
-prev_time=""
-for line in open('text.txt', 'r'):
-	time = line[11:16]
-	if prev_time != time:
-		print line.rstrip()
-		prev_time = time
-
