1efc311faac44e5c809a054ceb61a0960aa47a80
HKMR <hirojik@yahoo.co.jp>
2014-02-18 14:41:29 +0900

    indexを表示するように

---
+++ PYTHON/helloworld/guestbook.py
@@ -0,0 +1,29 @@
+#coding:UTF-8
+import os
+import urllib
+
+import jinja2
+import webapp2
+import datetime
+import locale
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+
+	def get(self):
+
+		template_values = {
+			'msg': u"こんにちは"
+		}
+
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
